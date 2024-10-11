0b6affd2f6ba339f3b6acd1118086ce0df4b74f5
Dymitr Baranowski <125156435+dymitrbaranowski@users.noreply.github.com>
2024-10-11 13:08:39 +0300

    finished

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import "./sass/main.scss";
+import tesla from "./assets/Tesla.jpg";
+
+const mainImage = document.getElementById("mainImage");
+mainImage.src = tesla;
+
+const hello = require("./js/hello");
